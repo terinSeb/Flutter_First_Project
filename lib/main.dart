8bf712534e33b07568c94dc97235f9131ad5b5aa
Terin Sebastin <terin01@gmail.com>
2022-05-06 02:26:34 +0100

    Added Button Events

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,29 @@ runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+
   const MyApp({Key? key}) : super(key: key);
+  void answerQuestion(){
+  debugPrint('Answer Choosen');
+}
 
   @override
   Widget build(BuildContext context){
     return   MaterialApp(home: Scaffold(
       appBar: AppBar(title: const Text('My First App'),),
-      body: const Text('This is My default App Demo'),
+      body: Column(
+        children:  [
+          const Text('The Question'),
+          ElevatedButton(child: const Text('Answer 1'),
+          onPressed: answerQuestion,),
+           ElevatedButton(child: const Text('Answer 2'),
+          onPressed: ()=> debugPrint('Answer Choosen 2') ,),
+           ElevatedButton(child: const Text('Answer 3'),
+          onPressed: () {
+            debugPrint('Answer Choosen 3');
+          },)
+        ],
+      ),
       ), );
   }
 }
